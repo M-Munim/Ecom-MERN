3c8651dd1acbb29ab6b5ef568f40ec8ec38f4c93
munim <muhammadmunimoff330@gmail.com>
2023-11-28 15:12:15 +0500

    hero jsx done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,15 +16,31 @@ const Navbar = () => {
             </div>
 
             <ul className='nav-menu'>
-                <li onClick={() => { setMenu("shop") }} > <Link style={{ textDecoration: 'none' }} to='/'> shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("mens") }} > <Link style={{ textDecoration: 'none' }} to='/mens'> men</Link>{menu === "mens" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("womens") }} > <Link style={{ textDecoration: 'none' }} to='/womens'> women</Link>{menu === "womens" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("kids") }} > <Link style={{ textDecoration: 'none' }} to='/kids'> kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("shop") }} >
+                    <Link style={{ textDecoration: 'none' }} to='/'> shop</Link>
+                    {menu === "shop" ? <hr /> : <></>}
+                </li>
+                <li onClick={() => { setMenu("mens") }} >
+                    <Link style={{ textDecoration: 'none' }} to='/mens'> men</Link>
+                    {menu === "mens" ? <hr /> : <></>}
+                </li>
+                <li onClick={() => { setMenu("womens") }} >
+                    <Link style={{ textDecoration: 'none' }} to='/womens'> women</Link>
+                    {menu === "womens" ? <hr /> : <></>}
+                </li>
+                <li onClick={() => { setMenu("kids") }} >
+                    <Link style={{ textDecoration: 'none' }} to='/kids'> kids</Link>
+                    {menu === "kids" ? <hr /> : <></>}
+                </li>
             </ul>
 
             <div className="nav-login-cart">
-                <Link to="/login"><button>Login</button></Link>
-                <Link to="/cart"><img src={cart_icon} alt="cart_Img" /></Link>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+                <Link to="/cart">
+                    <img src={cart_icon} alt="cart_Img" />
+                </Link>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>
