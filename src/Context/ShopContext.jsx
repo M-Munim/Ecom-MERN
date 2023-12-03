bd0db7fb211c98405eb7a877bf2041dd64f1119e
munim <muhammadmunimoff330@gmail.com>
2023-12-03 17:07:17 +0500

    some additions

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,15 +1,20 @@
 import React, { createContext } from "react";
 import all_products from "../Components/Assets/all_product"
 
+// created context and initialized with null
 export const ShopContext = createContext(null);
 
+
 const ShopContextprovider = (props) => {
+
     const contextValue = { all_products };
 
     return (
+        // returnig shopContext val from shopContextProvider
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
     )
 }
 
+export default ShopContextprovider;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ShopContextprovider from './Context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ShopContextprovider>
     <App />
-  </React.StrictMode>,
+  </ShopContextprovider>
+  // </React.StrictMode>,
 )
